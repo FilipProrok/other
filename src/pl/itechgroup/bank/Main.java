b440bad5217ae230ab668adcae210be34168dfca
ZSE <filip.prorok@gmail.com>
2021-10-25 11:03:40 +0200

    Data types added

--- src/pl/itechgroup/bank/Main.java
+++ src/pl/itechgroup/bank/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Witaj");
-        //boolean
+        boolean var1 = true;
+        char var2 = 'a';
+        byte var3 = 60;
+        short var4 = 180;
+        int var5 = 290;
+        long var6 = 555_444_333L;
+        float var7 = 4.4829F;
+        double var8 = 6.33335555;
+
     }
 }
